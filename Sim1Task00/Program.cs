fe160623ee9c4948086afdc6765336b6298df917
Ivan Borisenko <myivanbor@gmail.com>
2023-03-23 23:59:00 +0400

    Фиксируем начало работы

--- Sim1Task00/Program.cs
+++ Sim1Task00/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Начало семинара: Знакомство с языками программирования
+Console.WriteLine("Hello, Seminar!");
---
+++ Sim1Task04/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 5
+Console.WriteLine("Hello, World! 04");
+
+//Выводим запрос на ввод числа
+Console.Write("Введите число: ");
+//Считываем данные с консоли
+string? numLine = Console.ReadLine(); //операционная система общается строками, поэтому int сразу не пишем
+
+if(numLine!=null)
+{
+    int numN = int.Parse(numLine);
+
+    int startNum = numN*(-1);
+
+    string res = string.Empty;
+    
+    while(startNum <= numN)
+    {
+        res = res + startNum + " ";
+        startNum++;
+    }
+
+    Console.WriteLine(res);
+
+}
