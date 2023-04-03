1623c753ef2b9378680150a7a905915b48e0423c
Ivan Borisenko <myivanbor@gmail.com>
2023-04-03 11:12:52 +0400

    Фиксируем изменения Задачи № 10, 13, 13*, 15, 15*

--- Sem2_Task13hard/Program.cs
+++ Sem2_Task13hard/Program.cs
@@ -1,13 +1,16 @@
 ﻿// Задача № 13hard (не использовать char)
 // Вывод на консоль третьей цифры введенного числа (не более 10 знаков!)
 
+Console.WriteLine("\n Задача № 13hard ");
+Console.WriteLine(" Вывод на консоль третьей цифры многозначного числа");
+
 start:
 Console.WriteLine("  Введите число не менее 3-х и не более 10-и знаков:");
 
 // Проверяем является ли строковая переменная числом?
 if (int.TryParse(Console.ReadLine()!, out int nunLine))
 {
-    // Проверяем число на: не менее 3-х и не более 10-и знаков!
+    // Проверяем на: не менее 3-х и не более 10-и знаков в числе!
     // Число не должно начинаться с нуля!
     if (nunLine > 999999999 || nunLine < 100)
     {
@@ -15,9 +18,13 @@ if (int.TryParse(Console.ReadLine()!, out int nunLine))
         goto start;
     }
     // Находим третью цифру слева.
-    int Third(int nunLine)
+    // Делим без остатка на 10 в цикле, на выходе - сохраняем остаток от деления на 10.
+    int Third (int nunLine)
     {
-        while (nunLine >= 1000) nunLine /= 10;
+        while (nunLine >= 1000)
+        {
+            nunLine /= 10;
+        }
         int n = nunLine % 10;
         return n;
     }
--- Sem2_Task15hard/Program.cs
+++ Sem2_Task15hard/Program.cs
@@ -4,8 +4,11 @@
 Console.WriteLine("\n Задача № 15 Dictionary");
 Console.WriteLine(" Вводим число дня недели и проверяем является ли этот день выходным?\n");
 
-Dictionary <int, string> daysOfWeek = new Dictionary<int, string>();
+// Вызываем хэшированный словарь Dictionary
+// Задаем параметры ключей: TKey и TVale 
+Dictionary<int, string> daysOfWeek = new Dictionary<int, string>();
 
+// Определяем локальную переменную:
 daysOfWeek.Add(1, "Будний день");
 daysOfWeek.Add(2, "Будний день");
 daysOfWeek.Add(3, "Будний день");
@@ -14,15 +17,20 @@ daysOfWeek.Add(5, "Будний день");
 daysOfWeek.Add(6, "Выходной");
 daysOfWeek.Add(7, "Выходной");
 
-Console.Write(" Введите день недели (1-7): ");
+start:
+Console.WriteLine(" Введите день недели (1-7): ");
 
+// Вводим число дня недели:
 int day = int.Parse(Console.ReadLine() ?? "0");
 
+// Проверяем является ли введенное число днем недели?
+// Обращаемся к Dictionary
 if (daysOfWeek.ContainsKey(day))
 {
-Console.WriteLine(daysOfWeek[day]);
+    Console.WriteLine(daysOfWeek[day]);
 }
 else
 {
-Console.WriteLine(" Это не день недели!");
+    Console.WriteLine(" Это не день недели!");
+    goto start;
 }
